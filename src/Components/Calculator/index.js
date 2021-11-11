8ab5f0b2d8c0fc06894119124dadfe4674a87701
Daniel Mafra <danielmafradev@gmail.com>
2021-11-11 15:45:39 -0300

    Create initial component

---
+++ src/Components/Calculator/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './index.module.css';
+
+const Calculator = () => {
+  return (
+    <div>
+      Calculator
+    </div>
+  )
+}
+
+export default Calculator;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import styles from './Home.module.css';
+import Calculator from './Calculator/index'
 
 const Home = () => {
 
   return (
-    <div>
-      Tip calculator app
+    <div className={styles.home}>
+      <img src="/assets/logo.svg" alt="Splitter" />
+      <Calculator />
     </div>
   );
 };
