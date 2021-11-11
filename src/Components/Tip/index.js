4f3fde8591cccddf0adabafb143a3935bef6ee81
Daniel Mafra <danielmafradev@gmail.com>
2021-11-11 16:47:19 -0300

    Created tips selection

--- src/Components/Tip/index.js
+++ src/Components/Tip/index.js
@@ -3,8 +3,16 @@ import styles from './index.module.css';
 
 const Tip = () => {
   return (
-    <div>
-      Tip
+    <div className={styles.tips}>
+      <label>Select Tip %</label>
+      <div className={styles.percents}>
+        <button>5%</button>
+        <button>10%</button>
+        <button>15%</button>
+        <button>25%</button>
+        <button>50%</button>
+        <input type="number" placeholder="Custom" />
+      </div>
     </div>
   )
 }
